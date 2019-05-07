b32a8635a3a7eeb372b8bcc69cdaf53c9d4b71c2
ArdaHakan <50369789+ArdaHakan@users.noreply.github.com>
2019-05-07 17:52:20 +0200

    Add New Unity Project

---
+++ SchnitzeljagdUnity/Assets/Game/Knight/scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            anim.Play("StandingGreeting");
+        }
+        if (Input.GetKeyDown("2"))
+        {
+            anim.Play("Talking");
+        }
+        if (Input.GetKeyDown("3"))
+        {
+            anim.Play("Talking02");
+        }
+        if (Input.GetKeyDown("4"))
+        {
+            anim.Play("Talking03");
+        }
+        if (Input.GetKeyDown("5"))
+        {
+            anim.Play("Talking04");
+        }
+        if (Input.GetKeyDown("6"))
+        {
+            anim.Play("Talking05");
+        }
+        if (Input.GetKeyDown("7"))
+        {
+            anim.Play("Pointing");
+        }
+        if (Input.GetKeyDown("8"))
+        {
+            anim.Play("Defeated");
+        }
+        if (Input.GetKeyDown("9"))
+        {
+            anim.Play("LookingAround");
+        }
+        if (Input.GetKeyDown("0"))
+        {
+            anim.Play("ChickenDance");
+        }
+    }
+}
