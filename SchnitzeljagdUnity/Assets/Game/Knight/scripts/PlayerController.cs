cedafa00cc3501d442686fc91cd8c125bc5f43a6
Hakan Arda <50369789+ArdaHakan@users.noreply.github.com>
2019-05-10 21:13:30 +0200

    Add Task Maze

--- SchnitzeljagdUnity/Assets/Game/Knight/scripts/PlayerController.cs
+++ SchnitzeljagdUnity/Assets/Game/Knight/scripts/PlayerController.cs
@@ -1,19 +1,43 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody rb;
     public Animator anim;
+    
+
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Movement Functions
+        float x = CrossPlatformInputManager.GetAxis("Horizontal");
+        float y = CrossPlatformInputManager.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(x, 0, y);
+
+        rb.velocity = movement * 1f;
+
+        if(x != 0 && y != 0)
+        {
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.Atan2(x, y) * Mathf.Rad2Deg, transform.eulerAngles.z);
+        }
+        if(x != 0 || y != 0)
+        {
+            anim.Play("Walking");
+        }
+
+        //Animation Functios
         if (Input.GetKeyDown("1"))
         {
             anim.Play("StandingGreeting");
---
+++ SchnitzeljagdUnity/Assets/MazeRun/Scripts/camEventManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class camEventManager : MonoBehaviour
+{
+    public Camera[] cams;
+
+    public Button[] buttons;
+
+    
+
+    
+    public void camArMove()
+    {
+        buttons[0].interactable = false;
+        buttons[1].interactable = true;
+
+        cams[0].enabled = true;
+        cams[1].enabled = false;
+    }
+    public void camFpMove()
+    {
+        buttons[0].interactable = true;
+        buttons[1].interactable = false;
+
+        cams[0].enabled = false;
+        cams[1].enabled = true;
+    }
+}
