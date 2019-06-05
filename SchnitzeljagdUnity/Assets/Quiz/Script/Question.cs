d12dc82f01f52266ec1270042d1d099f74e4a6cf
Henneberger <henneberger.am@gmail.com>
2019-06-05 19:10:38 +0200

    Bugfixes Quiz

---
+++ SchnitzeljagdUnity/Assets/Quiz/Script/Question.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class Question {
+
+    //attributes
+    [SerializeField] private string info;
+    [SerializeField] private Answer[] answers;
+
+    //constructor
+    public Question(string info, Answer[] answers) {
+        Info = info;
+        Answers = answers;
+    }
+    //getter and setter
+    public string Info {
+        get { return info; }
+        set { info = value; }
+    }
+    public Answer[] Answers {
+        get { return answers; }
+        set { answers = value; }
+    }
+
+    //nested class
+    [System.Serializable]
+    public class Answer {
+        //attributes
+        [SerializeField] private string info;
+        [SerializeField] private bool isCorrect = false;
+
+        //constructor
+        public Answer(string info, bool isCorrect) {
+            Info = info;
+            IsCorrect = isCorrect;
+        }
+        //getter and setter
+        public string Info {
+            get { return info; }
+            set { info = value; }
+        }
+        public bool IsCorrect {
+            get { return isCorrect; }
+            set { isCorrect = value; }
+        }
+    }
+}
