498bca5a89575215cb1a8cce35cf3010b40cc2bd
Henneberger <henneberger.am@gmail.com>
2019-07-21 22:28:15 +0200

    Test Setup

--- SchnitzeljagdUnity/Assets/Scripts/Editor/MyGeometryTests.cs
+++ SchnitzeljagdUnity/Assets/Scripts/Editor/MyGeometryTests.cs
@@ -8,14 +8,17 @@ public class MyGeometryTests {
     [Test]
     public void TestIsWithinAngle() {
 
-        //// Arrange
-        //Vector3 from = new Vector3()
+        // Arrange
+        Vector3 from = new Vector3(1, 0, 1);
+        Vector3 to = new Vector3(1, 0, 1);
 
-        //// Act
-        //bool isWithinAngle = MyGeometry.IsWithinAngle();
+        // Act
+        bool isWithinAngle = MyGeometry.IsWithinAngle(from, to, -45, 45);
+        bool isNotWithinAngle = MyGeometry.IsWithinAngle(from, to, 45, -45);
 
-        //// Assert
-
+        // Assert
+        Assert.True(isWithinAngle);
+        Assert.False(isNotWithinAngle);
     }
 
 }
